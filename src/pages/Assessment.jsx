1833f9898a2d5c380d19bf7f9f0fd7e073b3cde8
Kennison7 <154489067+Kennison7@users.noreply.github.com>
2026-08-07 12:25:10 +0000

    fix: replace destructuring swap with temp variable

--- src/pages/Assessment.jsx
+++ src/pages/Assessment.jsx
@@ -9,7 +9,7 @@ function shuffleArray(array) {
   const arr = [...array]
   for (let i = arr.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
-    ;[arr[i], arr[j]] = [arr[j]], arr[i]]
+    const temp = arr[i]; arr[i] = arr[j]; arr[j] = temp
   }
   return arr
 }
